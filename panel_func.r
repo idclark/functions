de5bfdcf3290786e8945652494b88d7cc697ddb6
Ian Clark <idclark13@gmail.com>
2011-11-05 20:37:55 -0400

    table added

--- panel_func.r
+++ panel_func.r
@@ -1,20 +1,32 @@
 #writing our own ols regression function
-## should probably rename this
 
+
+#Example data############################################################
 data(women)
 x <- as.matrix(women$height)
 y <- as.matrix(women$weight)
-e <- as.matrix(1,15)
+e <- matrix(1,15)
 x <- cbind(e,x)
-# example data, shoul try it with others.
+# example data. You need to cbind() vector of ones
+# for global intercept for functions below. 
 
 ols.reg <- function(x,y){
   
-  coef <- solve(t(x)%*%x%*%t(x)%*%y)
-  print("coef estimates")
-  return(coef)
-  print(coef)
+  coef <- solve(t(x) %*% x) %*% (t(x) %*% y)
+  
+  e.ols <- y - x %*% coef
+  ssq.ols <-  (t(e.ols) %*% e.ols) / (nrow(coef) - ncol(x))
+  varcov.ols <- ssq.ols * (solve(t(x) %*% x))
+  se.ols <-  sqrt(diag(varcov.ols))
+  
+  t <- coef / se.ols
+  
+  table <- cbind(coef, se.ols, t)
+  print("OLS Estimates")
+  print(round(table,4))  
 }
 
-#add standard errors, p, t scores. 
+
+
+
 
