fac341c79236852105d149b603b38d19ca44c0cc
Ian Clark <idclark13@gmail.com>
2011-11-05 20:56:08 -0400

    fixed effects

--- panel_func.r
+++ panel_func.r
@@ -5,13 +5,38 @@
 data(women)
 x <- as.matrix(women$height)
 y <- as.matrix(women$weight)
+# some useful variables and matrices
+
+n <-
+t <-
+nt <- n * 
+# temporal element, how many units  and time observations
+
+I.t <- diag(1,t,t)
+# Identity matrix for t
+
 e <- matrix(1,15)
+#
+
+J.t <-  e %*% t(e)
+
+qmat <- I.t - 1 / t * J.t
+
+I.nt <- diag(1, n*t , n*t)
+
+I.n <- diag(1,n,n)
+
+k <- ncol(x[-1])
+
 x <- cbind(e,x)
 # example data. You need to cbind() vector of ones
-# for global intercept for functions below. 
+# for global intercept for functions below. hence cbind(e,x) now have a matrix
+#of explamataory variables and global intercept.
+
+
 
 ols.reg <- function(x,y){
-  
+#oridinary least aquares regression 
   coef <- solve(t(x) %*% x) %*% (t(x) %*% y)
   
   e.ols <- y - x %*% coef
@@ -26,7 +51,24 @@ ols.reg <- function(x,y){
   print(round(table,4))  
 }
 
+within.fix <- function(x,y){
+  #within group estimator for fixed effects
+  inxx.w <- solve(t(x) %*% qmatall %*% x)
+  b.w <- inxx.w %*% t(x) %*% qmatall %*% y
 
+  y.tilde <- as.matrix(qmatall %*% y)
+  x.tilde <- as.matrix(qmatall %*% x)
+  #FE Standard errors
+
+  e.tilde <- as.matrix(y.tide - (x.tilde %*% b.w))
+  ssq.fe <- (t(e.tilde) %*% e.tilde) / (nt- n- k )
+  se.w <- sqrt(diag(as.numeric( ssq.fe) * inxx.w))
+  t.w <- b.w / se.w
+
+  fetable <- cbind(b.w, se.w, t)
+  print("FE Estimates")
+  print(round(fetable, 4))
+}
 
 
 
